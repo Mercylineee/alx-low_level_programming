2a41b3367e2e201452928751a7eacdeeb6c9b789
Mercylineee <mercylinebuyaki@gmail.com>
2023-08-10 19:56:47 -0700

    Prints a line of text to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
